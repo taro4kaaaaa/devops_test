db7a87e5358416279904c67945111e125a9a96d3
Konstantin Tarasov <konstantin@Konstantins-MacBook-Pro.local>
2026-06-29 15:25:52 +0300

    Добавлен HTTP-сервер на Python

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,23 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-class Handler(BaseHTTPRequestHandler):
+class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello from Effective Mobile!")
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello from Effective Mobile!")
+        else:
+            self.send_response(404)
+            self.end_headers()
 
 
-server = HTTPServer(("0.0.0.0", 8080), Handler)
-server.serve_forever()
+def run():
+    server = HTTPServer(("0.0.0.0", 8080), RequestHandler)
+    print("Server started on port 8080")
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
